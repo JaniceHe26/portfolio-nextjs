82fd80673e4d7a395ec0c9d2a68225547d5381ec
Janice He <aikeiti@dominics-mbp.myfiosgateway.com>
2021-02-04 00:53:41 -0500

    added images

--- src/components/AboutComp/AboutComp.tsx
+++ src/components/AboutComp/AboutComp.tsx
@@ -1,15 +1,15 @@
 import React, { ReactElement } from 'react';
+import Image from 'next/image';
 
 export default function AboutComp(): ReactElement {
   return (
     <section className='about-container'>
-      <img className='img-profile' src='https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop'
-        alt='image of a cat' />
+      <Image src="/static/assets/images/janice.png" alt="Janice's profile picture" width={300} height={300} className='profile-img' />
       <div>
       {/* onHover for a more human translation later, use this later
           https://upmostly.com/tutorials/react-onhover-event-handling-with-examples */}
         <h2>hello, world</h2>
-        <h1>i am janice, a frontend developer by night and presales consultant by day</h1>
+        <h1>i am janice, a frontend developer by night and solutions consultant by day</h1>
         <p>i love learning new things and stuff from the INTERNweb and other hoomans</p>
         <p>then i use that big brain knowledge to kahsdjhaksjdfhkjlashdf a creative application</p>
         <p>when i am not coding, learning, overthinking, memeing, listening to moosic, or rewatching f.r.i.e.n.d.s for the 999999^th x</p>
